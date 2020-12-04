0a98857aec60234af7a522e7d6a36dc69bbf497d
StephWood <60517001+StephWood@users.noreply.github.com>
2020-12-04 12:14:26 -0800

    basic functionality created

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [currentSum, setCurrentSum] = useState(0);
+  const [clear, setClear] = useState(false);
+
+  // useEffect(() => {
+  //   document.getElementById('result').value = '';
+  // });
+
+  useEffect(() => {
+    if (clear) document.getElementById('result').value = '';
+  });
+
+  const Add = (e) => {
+    e.preventDefault();
+    if (clear) setClear(false);
+    let currentNum = document.getElementById('num').value;
+    if (currentNum == '') return;
+    let sum = currentSum + parseInt(currentNum);
+    setCurrentSum(sum);
+    document.getElementById('num').value = '';
+  };
+
+  const Subtract = (e) => {
+    e.preventDefault();
+    if (clear) setClear(false);
+    let currentNum = document.getElementById('num').value;
+    if (currentNum == '') return;
+    let sum = currentSum - parseInt(currentNum);
+    setCurrentSum(sum);
+    document.getElementById('num').value = '';
+  };
+
+  const Clear = (e) => {
+    e.preventDefault();
+    document.querySelector('form').reset();
+    setClear(true);
+    setCurrentSum(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-title">
+        <h1>Simple Calculator</h1>
+      </div>
+      <form>
+        <input type="text" id="num" placeholder="enter a number" />
+        <br />
+        <input type="text" id="result" value={currentSum} readOnly />
+        <br />
+        <button onClick={Add}>+</button>
+        <button onClick={Subtract}>-</button>
+        <button onClick={Clear}>Clr</button>
+      </form>
     </div>
   );
 }
